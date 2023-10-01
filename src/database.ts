c2973e4665e5f42ca517fe50b852beac33e67efe
Erick Gomes <erickgomes2905@gmail.com>
2023-10-01 10:33:56 -0300

    Complete

---
+++ src/database.ts
@@ -0,0 +1,42 @@
+export const database = [
+  {
+    img: 'assets/ac-cover.jpg',
+    label: 'Novo',
+    type: 'Digital PS5',
+    price: '209,99',
+  },
+
+  {
+    img: 'assets/bt-1.jpg',
+    label: 'Novo',
+    type: 'Digital PS4',
+    price: '129,99',
+  },
+
+  {
+    img: 'assets/bt-4.jpg',
+    label: 'Novo',
+    type: 'Digital PS5',
+    price: '189,99',
+  },
+
+  {
+    img: 'assets/bt-5.jpg',
+    label: 'Novo',
+    type: 'Digital PS4, PS5',
+    price: '209,99',
+  },
+
+  {
+    img: 'assets/bt-bad-company-2.jpg',
+    label: 'Novo',
+    type: 'Digital PS5',
+    price: '229,99',
+  },
+  {
+    img: 'assets/bt-hardline.jpg',
+    label: 'Novo',
+    type: 'Digital PS4, PS5',
+    price: '209,99',
+  },
+];
